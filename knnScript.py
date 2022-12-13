6cdf5c3b126771dae27e6cfc835a9e0ffe252593
Jelena <53283141+Joks84@users.noreply.github.com>
2022-12-13 16:44:13 +0800

    knn accuracy and error rate

--- knnScript.py
+++ knnScript.py
@@ -2,6 +2,8 @@ from process_data import preprocess_data
 from sklearn import model_selection
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from matplotlib import pyplot as plt
 
 # images with multiple classes
 # images_array = preprocess_data.createMulticlassData("process_data/photos/mix", 15)
@@ -17,8 +19,40 @@ knn.fit(X_train, y_train)
 
 
 y_predict = knn.predict(X_test)
+
 print("Knn score: ", knn.score(X_test, y_test))
 
 print("Confusion matrix: ", metrics.confusion_matrix(y_test, y_predict))
 
 print("Classification report: ", metrics.classification_report(y_test, y_predict))
+
+error_rate = []
+accuracy = []
+for i in range(1, 30):
+    knn = KNeighborsClassifier(n_neighbors=i).fit(X_train, y_train)
+    y_predict = knn.predict(X_test)
+    error_rate.append(np.mean(y_predict != y_test))
+    accuracy.append(metrics.accuracy_score(y_test, y_predict))
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, 30), error_rate, color="blue", linestyle="dashed", marker="o", markerfacecolor="red", markersize=10)
+plt.title("Error rate vs K value")
+plt.xlabel("K value")
+plt.ylabel("Error rate")
+print("Minimum error: ", min(error_rate), " at K = ", (error_rate.index(min(error_rate)) + 1))
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, 30), accuracy, color="blue", linestyle="dashed", marker="o", markerfacecolor="red", markersize=10)
+plt.title("Accuracy vs K value")
+plt.xlabel("K value")
+plt.ylabel("Accuracy")
+print("Minimum accuracy: ", max(accuracy), " at K = ", (accuracy.index(max(accuracy)) + 1))
+plt.show()
+
+
+
+
+
+
